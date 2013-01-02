f3e988415f7fb6e555e820ebe52778acfee0417d
Mykhaylo Khodorev <ralfeus@gmail.com>
2013-01-02 14:10:29 +0100

    fixed

--- system/database/mysql.php
+++ system/database/mysql.php
@@ -18,7 +18,7 @@ final class MySQL {
   	}
 		
   	public function query($sql) {
-          $log = new Log(DIR_LOGS . '/error.log');
+          $log = new Log('/error.log');
           $log->write($sql);
 		$resource = mysql_query($sql, $this->connection);
 
