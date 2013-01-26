ca03ab5c143ddd8925c0435b237a7a65dd50f8b3
Artem Kaehm <stinol@mail.ru>
2013-01-26 18:53:26 +0100

    добавлена возможность влиять на выдачу в модуле "Последнее"

--- admin/language/english/module/latest.php
+++ admin/language/english/module/latest.php
@@ -16,6 +16,7 @@ $_['entry_image']         = 'Image (W x H):';
 $_['entry_layout']        = 'Layout:';
 $_['entry_position']      = 'Position:';
 $_['entry_status']        = 'Status:';
+$_['entry_category']      = 'Category:';
 $_['entry_sort_order']    = 'Sort Order:';
 
 // Error
