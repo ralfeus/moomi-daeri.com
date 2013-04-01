10bddd31268298ed4c21cc014a9820753c2a761b
Mykhaylo Khodorev <ralfeus@gmail.com>
2013-04-01 13:07:55 +0200

    Revert "Изменены шрифты, цвета текста у кнопок. Добавлены кнопки вверху для скачивания и прочее"
    This reverts commit 67647fa4fe65d19c6325f6821b7f264d7a9d97e7.

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+// HTTP
+define('HTTP_SERVER', '/moomidaeri/');
+define('HTTP_CATALOG', '/moomidaeri/');
+define('HTTP_IMAGE', '/moomidaeri/image/');
+
+// HTTPS
+define('HTTPS_SERVER', '/moomidaeri/');
+define('HTTPS_CATALOG', '/moomidaeri/');
+define('HTTPS_IMAGE', '/moomidaeri/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/artemkaehm/Sites/localhost/moomidaeri/catalog/');
+define('DIR_SYSTEM', '/Users/artemkaehm/Sites/localhost/moomidaeri/system/');
+define('DIR_DATABASE', '/Users/artemkaehm/Sites/localhost/moomidaeri/system/database/');
+define('DIR_LANGUAGE', '/Users/artemkaehm/Sites/localhost/moomidaeri/catalog/language/');
+define('DIR_TEMPLATE', '/Users/artemkaehm/Sites/localhost/moomidaeri/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/artemkaehm/Sites/localhost/moomidaeri/system/config/');
+define('DIR_IMAGE', '/Users/artemkaehm/Sites/localhost/moomidaeri/image/');
+define('DIR_CACHE', '/Users/artemkaehm/Sites/localhost/moomidaeri/system/cache/');
+define('DIR_DOWNLOAD', '/Users/artemkaehm/Sites/localhost/moomidaeri/download/');
+define('DIR_LOGS', '/Users/artemkaehm/Sites/localhost/moomidaeri/system/logs/');
+define('DIR_CATALOG', '/Users/artemkaehm/Sites/localhost/moomidaeri/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'moomidae_db');
+define('DB_PREFIX', '');
+
+define('GALLERY_PAGE_SIZE', 20);
+define('GALLERY_COLS', 5);
+define('GALLERY_ROWS', 4);
