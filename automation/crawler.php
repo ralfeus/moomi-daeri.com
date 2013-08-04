da81f55d774b5405244d60f8d6cc9ab92504954d
Mykhaylo Khodorev <ralfeus@gmail.com>
2013-08-04 19:32:45 +0200

    #391

--- automation/crawler.php
+++ automation/crawler.php
@@ -1,6 +1,8 @@
 <?php
 require_once("simple_html_dom.php");
 require_once('../config.php');
+require_once('missha.php');
+require_once('natureRepublic.php');
 
 function handleError($errno, $errstr, $errfile, $errline, array $errcontext)
 {
@@ -116,96 +118,6 @@ class Product
     }
 }
 
-class NatureRepublic extends ProductSource
-{
-    private function __construct() {}
-
-    public static function getInstance() {
-        if (!self::$instance)
-            self::$instance = new NatureRepublic();
-        return self::$instance;
-    }
-
-    private function fillDetails(Product $product) {
-        $html = $this->getHtmlDocument($product->url);
-        /// Get images
-        $items = $html->find('div.detail_imgView a[rel=thumbnail]');
-        foreach ($items as $item)
-            $product->images[] = $item->attr['href'];
-
-        /// Get description
-        $items = $html->find('div.jeon_ingredient>dl>dd.center_t');
-        $product->description = mb_convert_encoding(trim($items[0]->innertext), 'utf-8', 'euc-kr');
-    }
-
-    private function getCategoryProducts($categoryUrl)
-    {
-        $products = array();
-        $html = $this->getHtmlDocument($categoryUrl);
-        $pages = $html->find('a[href^=javascript:paging]');
-        $pagesNum =  sizeof($pages) + 1;
-        $matches = array();
-        for ($currPage = 1; $currPage <= $pagesNum; $currPage++)
-        {
-            echo "Page $currPage of $pagesNum\n"; $tmp = 1;
-            $items = $html->find('a[href^=/product/nr_prod_detail.jsp]');
-            foreach ($items as $item) {
-                echo date('H:i:s') . "\tItem " . $tmp++ . " of " . sizeof($items) . "\n";
-                //$matches = array();
-                //$pid = preg_match('/(?<=pid=).*(?=&)', $item->attr['href'], $matches) ? $matches[0] : null;
-                $product = new Product(
-                    $this,
-                    preg_match('/(?<=pid=).*?(?=&)/', $item->attr['href'], $matches) ? $matches[0] : null,
-                    mb_convert_encoding(trim($item->find('text', 0)->plaintext), 'utf-8', 'euc-kr'),
-                    'http://' . $this->getSite()->name . $item->attr['href'],
-                    $item->parent->first_child()->attr['src'],
-                    preg_replace('/\D+/', '', $item->find('.price', 0)->plaintext),
-                    null,
-                    0.25
-                );
-                if ($this->addProductToList($product, $products)) {
-                    if (sizeof($item->find('strike')))
-                        $product->promoPrice = preg_replace('/\D+/', '', $item->find('strike', 0)->next_sibling()->plaintext);
-                    self::fillDetails($product);
-                }
-//                $products[] = $product;
-            }
-            if ($currPage < $pagesNum)
-                $html = $this->getHtmlDocument($categoryUrl . '&sPage=' . ($currPage + 1));
-        }
-        echo "Got " . sizeof($products) . " products\n";
-        return $products;
-    }
-
-    private function getCategoryUrls()
-    {
-        $categories = array();
-        $html = $this->getHtmlDocument(self::getUrl());
-        $items = $html->find('a[href^=/category/nr_ctgr_list.jsp]');
-        foreach ($items as $categoryAElement)
-            $categories[] = 'http://www.naturerepublic.co.kr' . $categoryAElement->attr['href'];
-        return $categories;
-    }
-
-    public function getProducts()
-    {
-        echo date('Y-m-d H:i:s') . "\n";
-        $products = array();
-        $urls = self::getCategoryUrls(); $currCategory = 1;
-        foreach ($urls as $url) {
-            echo "Crawling " . $currCategory++ . " of " . sizeof($urls) . "\n";
-            $products = array_merge($products, self::getCategoryProducts($url));
-//            break;
-        }
-        echo "Totally found " . sizeof($products) . " products\n";
-        echo date('Y-m-d H:i:s') . " --- Finished\n";
-        return $products;
-    }
-
-    public function getSite() { return (object) array('id' => 1, 'name' => 'www.naturerepublic.co.kr'); }
-    public function getUrl() { return 'http://www.naturerepublic.co.kr/etc/sitemap.jsp'; }
-}
-
 class DatabaseManager
 {
     private $connection;
@@ -271,7 +183,8 @@ class DatabaseManager
         $statement = $this->connection->prepare($sql);
         foreach ($site->getProducts() as $product)
         {
-//            print_r($product);
+            print_r($product);
+            break;
             $statement->execute(array(
                 ':sourceSiteId' => $site->getSite()->id,
                 ':sourceUrl' => $product->url,
@@ -300,5 +213,6 @@ class DatabaseManager
     }
 }
 $startTime = time();
-DatabaseManager::getInstance()->addProducts(NatureRepublic::getInstance());
-DatabaseManager::getInstance()->cleanup($startTime);
+//DatabaseManager::getInstance()->addProducts(NatureRepublic::getInstance());
+DatabaseManager::getInstance()->addProducts(Missha::getInstance());
+//DatabaseManager::getInstance()->cleanup($startTime);
---
+++ automation/missha.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: dev
+ * Date: 4.8.13
+ * Time: 15:00
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Missha extends ProductSource {
+    private function __construct() {}
+
+    private function fillDetails(Product $product) {
+        $html = $this->getHtmlDocument($product->url);
+        $matches = array();
+        /// Get images
+        $items = $html->find('td.M_s_img_b img');
+        foreach ($items as $item)
+            if (preg_match('/(?<=loadImage\(\').+?(?=\'\))', $item->attr['onmouseover'], $matches))
+                $product->images[] =  $matches[0];
+
+        /// Get description
+        $items = $html->find('td.M_g_detail');
+        $product->description = mb_convert_encoding(trim($items[0]->innertext), 'utf-8', 'euc-kr');
+    }
+
+    private function getCategoryProducts($categoryUrl)
+    {
+        $products = array();
+        echo $categoryUrl . "\n";
+        $html = $this->getHtmlDocument($categoryUrl);
+        $pages = $html->find('div.pg2 a[href*=category_large.php\?pagenum]');
+        $pagesNum =  sizeof($pages) + 1;
+        $matches = array();
+        for ($currPage = 1; $currPage <= $pagesNum; $currPage++)
+        {
+            echo "Page $currPage of $pagesNum\n"; $tmp = 1;
+            $items = $html->find('table.news03>tbody>tr>td>table>tbody');
+            foreach ($items as $item) {
+                echo date('H:i:s') . "\tItem " . $tmp++ . " of " . sizeof($items) . "\n";
+                $aElement = $item->children(1)->find('a[href*=category_detail.php]', 0);
+                $product = new Product(
+                    $this,
+                    preg_match('/(?<=id=)\d*?/', $aElement->attr['href'], $matches) ? $matches[0] : null,
+                    mb_convert_encoding(trim($item->children(2)->find('a[href*=category_detail.php]', 0)->find('text', 0)->plaintext), 'utf-8', 'euc-kr'),
+                    'http://shop.beautynet.co.kr/' . $aElement->attr['href'],
+                    $aElement->find('image', 0)->attr['src'],
+                    preg_replace('/\D+/', '', $item->children(2)->find('td.won', 0)->plaintext),
+                    null,
+                    0.25
+                );
+                if ($this->addProductToList($product, $products)) {
+                    if (sizeof($item->find('strike')))
+                        $product->promoPrice = preg_replace('/\D+/', '', $item->find('strike', 0)->next_sibling()->plaintext);
+                    self::fillDetails($product);
+                }
+//                $products[] = $product;
+            }
+            if ($currPage < $pagesNum)
+                $html = $this->getHtmlDocument($categoryUrl . '&pagenum=' . ($currPage + 1));
+        }
+        echo "Got " . sizeof($products) . " products\n";
+        return $products;
+    }
+
+    private function getCategoryUrls()
+    {
+        $categories = array();
+        $html = $this->getHtmlDocument(self::getUrl());
+        $items = $html->find('a[href*=\/missha\/category_large.php]');
+        foreach ($items as $categoryAElement)
+            $categories[] = 'http://shop.beautynet.co.kr/' . $categoryAElement->attr['href'];
+        return array_unique($categories);
+    }
+
+    public function getProducts() {
+        echo date('Y-m-d H:i:s') . "\n";
+        $products = array();
+        $urls = self::getCategoryUrls(); $currCategory = 1;
+        foreach ($urls as $url) {
+            echo "Crawling " . $currCategory++ . " of " . sizeof($urls) . "\n";
+            $products = array_merge($products, self::getCategoryProducts($url));
+            break;
+        }
+        echo "Totally found " . sizeof($products) . " products\n";
+        echo date('Y-m-d H:i:s') . " --- Finished\n";
+        return $products;
+    }
+
+    /**
+     * @return ProductSource
+     */
+    public static function getInstance() {
+        if (!self::$instance)
+            self::$instance = new Missha();
+        return self::$instance;
+    }
+
+    /**
+     * @return stdClass
+     */
+    public function getSite() { return (object)array( 'id' => 2, 'name' => 'shop.beautynet.co.kr/missha/'); }
+
+    public function getUrl() { return 'http://shop.beautynet.co.kr/missha/category_large.php?part_code=115001001000'; }
+}
