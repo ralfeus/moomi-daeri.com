9e049b64016e63a70602d7ee0218f06c7c4dc3db
Mykhaylo Khodorev <ralfeus@gmail.com>
2013-08-06 11:38:35 +0200

    #389 (manual start)

--- automation/startImport.php
+++ automation/startImport.php
@@ -6,4 +6,4 @@
  * Time: 10:27
  * To change this template use File | Settings | File Templates.
  */
-echo shell_exec('ps axo cmd | grep "php crawler.php"');
+echo shell_exec('ps axo cmd | grep "^php crawler.php"');
