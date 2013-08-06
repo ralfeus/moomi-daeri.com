ec0cb238b5dea931c5a3d448b975d0de4c978cbd
Mykhaylo Khodorev <ralfeus@gmail.com>
2013-08-06 11:51:40 +0200

    #389 (manual start)

--- automation/startImport.php
+++ automation/startImport.php
@@ -7,5 +7,5 @@
  * To change this template use File | Settings | File Templates.
  */
 if (!shell_exec('ps axo cmd | grep "^php crawler.php"')) {
-    shell_exec('cd /var/www/moomi-daeri.com/automation && nohup php crawler.php &');
+    exec('cd /var/www/moomi-daeri.com/automation && nohup php crawler.php &');
 }
