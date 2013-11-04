0e5af7d98b107c3a42d53efad62a2880f14564f3
Mykhaylo Khodorev <ralfeus@gmail.com>
2013-11-04 22:08:45 +0100

    APC implemented

--- system/library/cache.php
+++ system/library/cache.php
@@ -3,55 +3,75 @@ final class Cache {
 	private $expire = 3600; 
 
   	public function __construct() {
-		$files = glob(DIR_CACHE . 'cache.*');
-		
-		if ($files) {
-			foreach ($files as $file) {
-				$time = substr(strrchr($file, '.'), 1);
-
-      			if ($time < time()) {
-					if (file_exists($file)) {
-						unlink($file);
-						clearstatcache();
-					}
-      			}
-    		}
-		}
+//		$files = glob(DIR_CACHE . 'cache.*');
+//
+//		if ($files) {
+//			foreach ($files as $file) {
+//				$time = substr(strrchr($file, '.'), 1);
+//
+//      			if ($time < time()) {
+//					if (file_exists($file)) {
+//						unlink($file);
+//						clearstatcache();
+//					}
+//      			}
+//    		}
+//		}
   	}
 
-	public function get($key) {
-		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', md5($key)) . '.*');
+//	public function get($key) {
+//		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', md5($key)) . '.*');
+//
+//		if ($files) {
+//			$cache = file_get_contents($files[0]);
+//
+//			return unserialize($cache);
+//		}
+//	}
 
-		if ($files) {
-			$cache = file_get_contents($files[0]);
-			
-			return unserialize($cache);
-		}
-	}
+    /// New version with APC using
+    public function get($key) {
+        if (apc_exists($key)) {
+            return apc_fetch($key);
+        }
+        else {
+            return false;
+        }
+    }
 
-  	public function set($key, $value) {
-    	$this->delete($key);
-		
-		$file = DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', md5($key)) . '.' . (time() + $this->expire);
-    	
-		$handle = fopen($file, 'w');
+//  	public function set($key, $value) {
+//    	$this->delete($key);
+//
+//		$file = DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', md5($key)) . '.' . (time() + $this->expire);
+//
+//		$handle = fopen($file, 'w');
+//
+//    	fwrite($handle, serialize($value));
+//
+//    	fclose($handle);
+//  	}
 
-    	fwrite($handle, serialize($value));
-		
-    	fclose($handle);
-  	}
+    /// New version with APC using
+    public function set($key, $value) {
+        apc_store($key, $value);
+    }
 	
-  	public function delete($key) {
-		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', md5($key)) . '.*');
-		
-		if ($files) {
-    		foreach ($files as $file) {
-      			if (file_exists($file)) {
-					unlink($file);
-					clearstatcache();
-				}
-    		}
-		}
-  	}
+//  	public function delete($key) {
+//		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', md5($key)) . '.*');
+//
+//		if ($files) {
+//    		foreach ($files as $file) {
+//      			if (file_exists($file)) {
+//					unlink($file);
+//					clearstatcache();
+//				}
+//    		}
+//		}
+//  	}
+
+    /// New version with APC using
+    public function delete($key) {
+        apc_delete($key);
+    }
 }
 ?>
