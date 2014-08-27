65e5d21c03d670e2a24b7c1c6aa2117a3b276340
Mykhaylo Khodorev <ralfeus@gmail.com>
2014-08-27 12:42:15 +0200

    #41

--- automation/HolikaHolika.php
+++ automation/HolikaHolika.php
@@ -1,2 +1,23 @@
 <?php
- 
+require_once('gmarket.co.kr.php');
+class HolikaHolika extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'TE5NR38zOTgxOM32OTc0Mzk5NTJ/Rw==';
+    }
+
+    /**
+     * @return ProductSource
+     */
+    public static function getInstance() {
+        if (!self::$instance || !(self::$instance instanceof HolikaHolika))
+            self::$instance = new self();
+        return self::$instance;
+    }
+
+    /**
+     * @return stdClass
+     */
+    public function getSite() {
+        return (object)array( 'id' => 5, 'name' => 'HolikaHolika');
+    }
+}
