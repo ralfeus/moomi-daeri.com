031dcb78983a9af83b44ca1f98eb2b5700cf4626
Mykhaylo Khodorev <ralfeus@gmail.com>
2014-09-03 09:38:33 +0200

    #41

--- automation/NatureRepublic.php
+++ automation/NatureRepublic.php
@@ -1,5 +1,21 @@
 <?php
+require_once('gmarket.co.kr.php');
+class NatureRepublic extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'zEyOR38TOTAxMQwzMzYzMzk2NzF/Rw==';
+    }
 
-class NatureRepublic {
+    /**
+     * @return NatureRepublic
+     */
+    public static function getInstance() {
+        if (!self::$instance || !(self::$instance instanceof NatureRepublic))
+            self::$instance = new self();
+        return self::$instance;
+    }
 
-} 
+    /**
+     * @return stdClass
+     */
+    public function getSite() { return (object)array( 'id' => 1, 'name' => 'NatureRepublic'); }
+}
