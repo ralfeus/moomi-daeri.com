4c8ebdac45bd63698bccbbed71cf27a868fbd21b
Mykhaylo Khodorev <ralfeus@gmail.com>
2014-10-08 08:55:21 +0200

    add: Supplier::freeShippingThreshold

--- model/catalog/Supplier.class.php
+++ model/catalog/Supplier.class.php
@@ -8,18 +8,20 @@ class Supplier {
     private $name;
     private $internalModel;
     private $shippingCost;
+    private $freeShippingThreshold;
 
-    public function __construct($groupId, $id, $internalModel, $name, $shippingCost) {
-        $this->groupId = $groupId;
-        $this->id = $id;
-        $this->internalModel = $internalModel;
-        $this->name = $name;
-        $this->shippingCost = $shippingCost;
+    public function __construct($id, $groupId = null, $internalModel = null, $name = null, $shippingCost = null, $freeShippingThreshold = null) {
+        $this->id = $id; ///TODO: Перевірити, чи може товар існувати без постачальника
+        if (!is_null($groupId)) { $this->groupId = $groupId; }
+        if (!is_null($internalModel)) { $this->internalModel = $internalModel; }
+        if (!is_null($name)) { $this->name = $name; }
+        if (!is_null($shippingCost)) { $this->shippingCost = $shippingCost; }
+        if (!is_null($freeShippingThreshold)) { $this->freeShippingThreshold = $freeShippingThreshold ; }
     }
 
     public function getGroup() {
         if (!isset($this->group)) {
-            $this->group = SupplierGroupDAO::getInstance()->getSupplierGroup($this->groupId);
+            $this->group = SupplierGroupDAO::getInstance()->getSupplierGroup($this->getGroupId());
         }
         return $this->group;
     }
@@ -27,8 +29,10 @@ class Supplier {
     /**
      * @return mixed
      */
-    public function getGroupId()
-    {
+    public function getGroupId() {
+        if (!isset($this->groupId)) {
+            $this->groupId = SupplierDAO::getInstance()->getGroupId($this->id);
+        }
         return $this->groupId;
     }
 
@@ -43,6 +47,9 @@ class Supplier {
      * @return mixed
      */
     public function getInternalModel() {
+        if (!isset($this->internalModel)) {
+            $this->internalModel = SupplierDAO::getInstance()->getInternalModel($this->id);
+        }
         return $this->internalModel;
     }
 
@@ -50,6 +57,9 @@ class Supplier {
      * @return mixed
      */
     public function getName() {
+        if (!isset($this->name)) {
+            $this->name = SupplierDAO::getInstance()->getName($this->id);
+        }
         return $this->name;
     }
 
@@ -61,5 +71,15 @@ class Supplier {
             $this->shippingCost = SupplierDAO::getInstance()->getShippingCost($this->id);
         }
         return $this->shippingCost;
+    }
+
+    /**
+     * @return float
+     */
+    public function getFreeShippingThreshold() {
+        if (!isset($this->freeShippingThreshold)) {
+            $this->freeShippingThreshold = SupplierDAO::getInstance()->getFreeShippingThreshold($this->id);
+        }
+        return $this->freeShippingThreshold;
     }
 }
