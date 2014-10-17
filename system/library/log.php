7eb6213ea35974e22b84b352858b88e29258e00d
Mykhaylo Khodorev <ralfeus@gmail.com>
2014-10-17 18:31:56 +0200

    debug

--- system/library/log.php
+++ system/library/log.php
@@ -11,10 +11,11 @@ final class Log {
         array_shift($trace);
         $callerArray = $trace[0];
         $caller = isset($callerArray['class']) ? $callerArray['class'] . "::" . $callerArray['function'] : $callerArray['function'];
+        $line = isset($callerArray['line']) ? $callerArray['line'] : 0;
 
 		$file = DIR_LOGS . $this->filename;
 		$handle = fopen($file, 'a+');
-		fwrite($handle, date('Y-m-d G:i:s') . "\t$caller()\t" . $message . "\n");
+		fwrite($handle, date('Y-m-d G:i:s') . "\t$caller():$line\t" . $message . "\n");
 		fclose($handle);
 	}
 }
