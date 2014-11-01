e2f01fced29f150e3bc5c0bd6e61d1e45d4eb27e
Andrey Yugay <don.yerdna@gmail.com>
2014-11-01 14:39:58 +0300

    Added headercont.tpl for contentmanagers v5

--- admin/view/template/common/headercont.tpl.php
+++ admin/view/template/common/headercont.tpl.php
@@ -96,10 +96,11 @@ $(document).ready(function(){
       <li id="catalog"><a class="top"><?php echo $text_catalog; ?></a>
         <ul>
           <li><a href="<?php echo $category; ?>"><?php echo $text_category; ?></a></li>
-            <li><a class="parent" href="<?php echo $product; ?>"><?php echo $text_product; ?></a></li>
+            <li><a class="parent" href="<?php echo $product; ?>"><?php echo $text_product; ?></a>
               <ul>
                   <li><a href="<?php echo $product; ?>&resetFilter=1"><?php echo $text_reset_filter_product; ?></a></li>
               </ul>
+            </li>
             <li><a class="parent"><?php echo $text_attribute; ?></a>
               <ul>
                 <li><a href="<?php echo $attribute; ?>"><?php echo $text_attribute; ?></a></li>
