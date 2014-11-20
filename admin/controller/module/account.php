877e150cd7ce9dc58a553dcc5f4cbdb10518f545
Mykhaylo Khodorev <ralfeus@gmail.com>
2014-11-20 20:20:14 +0100

    modify: Controller::response -> Controller::getResponse()

--- admin/controller/module/account.php
+++ admin/controller/module/account.php
@@ -84,7 +84,7 @@ class ControllerModuleAccount extends Controller {
 			'common/footer'
 		);
 				
-		$this->response->setOutput($this->render());
+		$this->getResponse()->setOutput($this->render());
 	}
 	
 	private function validate() {
