3150080e10387a42b380463cc3f52a03fb352403
Mykhaylo Khodorev <ralfeus@gmail.com>
2015-03-09 23:03:00 +0100

    add: Chef import

---
+++ automation/SourceSite/Chef.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace automation\SourceSite;
+
+class Chef extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'DI1MR38TOTIxOU0yNzA5MDUxODJ/Rw==';
+    }
+
+//    /**
+//     * @return \stdClass
+//     */
+//    public function getSite() { return (object)array( 'id' => 12, 'name' => 'BanilaCO'); }
+}
