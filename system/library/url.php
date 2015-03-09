37a756cb9a555ae6189eea719908da2405fb0f47
Mykhaylo Khodorev <ralfeus@gmail.com>
2015-03-09 22:14:41 +0100

    revert: 3186fec

--- system/library/url.php
+++ system/library/url.php
@@ -11,34 +11,34 @@ class Url {
 	
 	public function link($route, $args = '', $connection = 'NONSSL') {
         
-        // SIMPLE START
-        $get_route = isset($_GET['route']) ? $_GET['route'] : (isset($_GET['_route_']) ? $_GET['_route_'] : '');
-            
-        if ($route == 'checkout/cart' && $get_route != 'checkout/cart') {
-            $connection = 'SSL';
-            $route = 'checkout/simplecheckout';
-        }
-        
-        if ($route == 'checkout/checkout' && $get_route != 'checkout/checkout') {
-            $route = 'checkout/simplecheckout';
-        }
-        
-        if ($route == 'account/register' && $get_route != 'account/register') {
-            $route = 'account/simpleregister';
-        }   
-
-        if ($route == 'account/edit' && $get_route != 'account/edit') {
-            $route = 'account/simpleedit';
-        }  
-
-        if ($route == 'account/address/update' && $get_route != 'account/address/update') {
-            $route = 'account/simpleaddress/update';
-        } 
-
-        if ($route == 'account/address/insert' && $get_route != 'account/address/insert') {
-            $route = 'account/simpleaddress/insert';
-        }       
-        // SIMPLE END
+//        // SIMPLE START
+//        $get_route = isset($_GET['route']) ? $_GET['route'] : (isset($_GET['_route_']) ? $_GET['_route_'] : '');
+//
+//        if ($route == 'checkout/cart' && $get_route != 'checkout/cart') {
+//            $connection = 'SSL';
+//            $route = 'checkout/simplecheckout';
+//        }
+//
+//        if ($route == 'checkout/checkout' && $get_route != 'checkout/checkout') {
+//            $route = 'checkout/simplecheckout';
+//        }
+//
+//        if ($route == 'account/register' && $get_route != 'account/register') {
+//            $route = 'account/simpleregister';
+//        }
+//
+//        if ($route == 'account/edit' && $get_route != 'account/edit') {
+//            $route = 'account/simpleedit';
+//        }
+//
+//        if ($route == 'account/address/update' && $get_route != 'account/address/update') {
+//            $route = 'account/simpleaddress/update';
+//        }
+//
+//        if ($route == 'account/address/insert' && $get_route != 'account/address/insert') {
+//            $route = 'account/simpleaddress/insert';
+//        }
+//        // SIMPLE END
         
 		if ($connection ==  'NONSSL') {
 			$url = $this->url;	
