1733c38b5039e83f7985307138ef3b0c8a1d30f5
Mykhaylo Khodorev <ralfeus@gmail.com>
2015-04-07 13:56:51 +0200

    fix: deposit messages

--- catalog/language/russian/account/reward.php
+++ catalog/language/russian/account/reward.php
@@ -1,15 +1,14 @@
 <?php
 // Heading 
-$_['heading_title']      = 'Бонусные баллы';
+$_['heading_title']      = 'Депозит';
 
 // Column
 $_['column_date_added']  = 'Добавлено';
 $_['column_description'] = 'Описание';
-$_['column_points']      = 'Бонусные баллы';
+$_['column_points']      = 'Депозит';
 
 // Text
 $_['text_account']       = 'Личный Кабинет';
-$_['text_reward']        = 'Бонусные баллы';
-$_['text_total']         = 'Накоплено бонусных баллов:';
-$_['text_empty']         = 'У Вас нет бонусных балов!';
-?>
+$_['text_reward']        = 'Депозит';
+$_['text_total']         = 'Всего:';
+$_['text_empty']         = 'Депозит пуст';
