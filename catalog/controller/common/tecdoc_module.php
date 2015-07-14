5031f4dff9e35e79147b67cc4878cbe86b4d3427
Mykhaylo Khodorev <ralfeus@gmail.com>
2015-07-14 14:23:33 +0200

    add: autoparts

--- catalog/controller/common/tecdoc_module.php
+++ catalog/controller/common/tecdoc_module.php
@@ -15,20 +15,34 @@ class Controllercommontecdocmodule extends Controller {
             $this->document->addLink(HTTP_SERVER, 'canonical');
         }
 
-        $data['column_left'] = $this->load->controller('common/column_left');
-        $data['column_right'] = $this->load->controller('common/column_right');
-        $data['content_top'] = $this->load->controller('common/content_top');
-        $data['content_bottom'] = $this->load->controller('common/content_bottom');
-        $data['footer'] = $this->load->controller('common/footer');
-        $data['header'] = $this->load->controller('common/header');
-
-
-        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/tecdoc_module.tpl')) {
-            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/tecdoc_module.tpl', $data));
+        $file = '/template/common/tecdoc_module.tpl';
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . $file)) {
+            $this->template = $this->config->get('config_template') . $file;
         } else {
-            $this->response->setOutput($this->load->view('default/template/common/tecdoc_module.tpl', $data));
+            $this->template = 'default' . $file;
         }
 
+//        $data['column_left'] = $this->load->controller('common/column_left');
+//        $data['column_right'] = $this->load->controller('common/column_right');
+//        $data['content_top'] = $this->load->controller('common/content_top');
+//        $data['content_bottom'] = $this->load->controller('common/content_bottom');
+//        $data['footer'] = $this->load->controller('common/footer');
+//        $data['header'] = $this->load->controller('common/header');
+        $this->children = array(
+            'common/column_left',
+            'common/column_right',
+            'common/content_top',
+            'common/content_bottom',
+            'common/footer',
+            'common/header'
+        );
 
+//
+//        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/tecdoc_module.tpl')) {
+//            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/tecdoc_module.tpl', $data));
+//        } else {
+//            $this->response->setOutput($this->load->view('default/template/common/tecdoc_module.tpl', $data));
+//        }
+        $this->getResponse()->setOutput($this->render());
     }
 }
