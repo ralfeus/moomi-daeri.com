153bed69ffd91c9c8ac38283d279cd2e72d98dbc
Mykhaylo Khodorev <ralfeus@gmail.com>
2015-10-08 11:33:14 +0200

    fix: minor issues

--- catalog/controller/shop/admin.php
+++ catalog/controller/shop/admin.php
@@ -127,16 +127,15 @@ class ControllerShopAdmin extends Controller {
     $this->load->model('shop/general');
     $result = $this->model_shop_general->getAction($data);
 
-    return $result[0];
+    return !empty($result) ? $result[0] : null;
   }
 
   public function object2array($obj) {
     $_arr = is_object($obj) ? get_object_vars($obj) : $obj;
     foreach ($_arr as $key => $val) {
-      $val = (is_array($val) || is_object($val)) ? object2array($val) : $val;
+      $val = (is_array($val) || is_object($val)) ? $this->object2array($val) : $val;
       $arr[$key] = $val;
     }
     return $arr;
   }
 }
-?>
