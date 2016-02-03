7892d03d8d44f178fa3b5b7437d25e2f6531e793
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-02-03 14:32:51 +0100

    add: Vivajane, AromaStory

---
+++ automation/SourceSite/AromaStory.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace automation\SourceSite;
+
+class AromaStory extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'TI2NR38jNTUxMY22Nzk0OTYyMDF/Rw==';
+    }
+
+//    /**
+//     * @return \stdClass
+//     */
+//    public function getSite() { return (object)array( 'id' => 12, 'name' => 'BanilaCO'); }
+}
---
+++ automation/SourceSite/Vivajane.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace automation\SourceSite;
+
+class Vivajane extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'zA0OR38DODIxNMw2OTk1MjAxMzR/Rw==';
+    }
+
+//    /**
+//     * @return \stdClass
+//     */
+//    public function getSite() { return (object)array( 'id' => 12, 'name' => 'BanilaCO'); }
+}
