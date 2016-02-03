bd609f93fe837e05e61ed3708a63edad56882e09
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-02-03 14:49:40 +0100

    add: Graymelin, TheBodyShop

---
+++ automation/SourceSite/Graymelin.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace automation\SourceSite;
+
+class Greymelin extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'jI5MR38DNjAxME1yMzYzNzkxNDR/Rw==';
+    }
+
+//    /**
+//     * @return \stdClass
+//     */
+//    public function getSite() { return (object)array( 'id' => 12, 'name' => 'BanilaCO'); }
+}
---
+++ automation/SourceSite/TheBodyShop.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace automation\SourceSite;
+
+class TheBodyShop extends GMarketCoKr {
+    public function __construct() {
+        $this->shopId = 'zAyMR38zODIxMg14MjIzMzIxODd/Rw==';
+    }
+
+//    /**
+//     * @return \stdClass
+//     */
+//    public function getSite() { return (object)array( 'id' => 12, 'name' => 'BanilaCO'); }
+}
