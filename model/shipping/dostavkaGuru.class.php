4bf300015ff448fa99f2e92e366a556144a2c62b
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-02-17 16:38:26 +0100

    fix: git issues

--- model/shipping/dostavkaGuru.class.php
+++ model/shipping/dostavkaGuru.class.php
@@ -4,36 +4,36 @@ use model\setting\SettingsDAO;
 use system\helper\WebClient;
 
 class DostavkaGuru extends ShippingMethodBase {
-	private $intermediateZoneId = 26; // EMS - сборная самовывоз
-	private $intermediateShippingMethod = 'ems';
-	private $partnerId = 9999; // Temp ID
-	private $key = '827ccb0eea8a706c4c34a16891f84e7b'; // Temp key
+    private $intermediateZoneId = 26; // EMS - сборная самовывоз
+    private $intermediateShippingMethod = 'ems';
+    private $partnerId = 9999; // Temp ID
+    private $key = '827ccb0eea8a706c4c34a16891f84e7b'; // Temp key
 
-	public function getAddress($shippingMethodCode) {
-		$pointId = explode('_', $shippingMethodCode)[1];
-		$response = WebClient::getResponse(
-			'http://api.dostavka.guru/client/pvz_list.php',
-			'POST',
-			[
-				'partner_id' => $this->partnerId,
-				'key' => $this->key,
-				'script' => 'all_list',
-				'pointID' => $pointId
-			]);
-		$xml = new \SimpleXMLElement($response);
-		$result = [
-			'address_1' => $xml->point->small_address->__toString(),
-			'address_2' => '',
-			'city' => $xml->point->city_name->__toString(),
-			'postcode' => explode(',', $xml->point->address->__toString())[0]
-		];
-		return $result;
-	}
+    public function getAddress($shippingMethodCode) {
+        $pointId = explode('_', $shippingMethodCode)[1];
+        $response = WebClient::getResponse(
+            'http://api.dostavka.guru/client/pvz_list.php',
+            'POST',
+            [
+                'partner_id' => $this->partnerId,
+                'key' => $this->key,
+                'script' => 'all_list',
+                'pointID' => $pointId
+            ]);
+        $xml = new \SimpleXMLElement($response);
+        $result = [
+            'address_1' => $xml->point->small_address->__toString(),
+            'address_2' => '',
+            'city' => $xml->point->city_name->__toString(),
+            'postcode' => explode(',', $xml->point->address->__toString())[0]
+        ];
+        return $result;
+    }
 
-	public function getCost($destination, $orderItems, $ext = array()) {
+    public function getCost($destination, $orderItems, $ext = array()) {
 //        $this->log->write(print_r($orderItems, true));
         $cost = 0;
-        $rates = explode(',', $this->config->get($destination . '_rate'));
+        $rates = explode(',', SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'intermediateZoneRate'));
         if (empty($ext['weight'])) {
             $totalWeight = 0;
             foreach ($orderItems as $orderItem) {
@@ -57,86 +57,52 @@ class DostavkaGuru extends ShippingMethodBase {
             }
         }
         $this->log->write($cost);
-		return $cost;
-  	}
+        return $cost;
+    }
 
-	/**
-	 * @param string[] $address
-	 * @return IssuePoint[]
-	 */
-	private function getIssuePoints($address) {
-		$response = WebClient::getResponse(
-			'http://api.dostavka.guru/client/pvz_list.php',
-			'POST',
-			[
-				'partner_id' => $this->partnerId,
-				'key' => $this->key,
-				'script' => 'all_list',
-				'city_name' => $address['city']
-			]);
-		$xml = new \SimpleXMLElement($response);
-		$result = [];
-		foreach ($xml->xpath('point') as $point) {
-			$issuePoint = new IssuePoint();
-			$issuePoint->name = $point->name->__toString();
-			$issuePoint->id = $point->code->__toString();
-			$issuePoint->address = $point->address->__toString();
-			$result[] = $issuePoint;
-		}
-		return $result;
-	}
+    /**
+     * @param string[] $address
+     * @return IssuePoint[]
+     */
+    private function getIssuePoints($address) {
+        $response = WebClient::getResponse(
+            'http://api.dostavka.guru/client/pvz_list.php',
+            'POST',
+            [
+                'partner_id' => $this->partnerId,
+                'key' => $this->key,
+                'script' => 'all_list',
+                'city_name' => $address['city']
+            ]);
+        $xml = new \SimpleXMLElement($response);
+        $result = [];
+        foreach ($xml->xpath('point') as $point) {
+            $issuePoint = new IssuePoint();
+            $issuePoint->name = $point->name->__toString();
+            $issuePoint->id = $point->code->__toString();
+            $issuePoint->address = $point->address->__toString();
+            $result[] = $issuePoint;
+        }
+        return $result;
+    }
 
     public function getMethodData($address) {
-//        $this->log->write(print_r($address, true));
-        $sql = "
-            SELECT gz.geo_zone_id, gz.name, gz.description
-            FROM
-                setting AS s
-                JOIN geo_zone AS gz ON `key` = concat('dostavkaGuru_', gz.geo_zone_id, '_status') AND value = 1
-                JOIN zone_to_geo_zone AS ztgz ON gz.geo_zone_id = ztgz.geo_zone_id
-            WHERE
-                ztgz.country_id = " . (int)$address['country_id'] . "
-                AND ztgz.zone_id IN (" . (int)$address['zone_id'] . ", 0)
-        ";
-        $this->log->write($sql);
-        $query = $this->getDb()->query($sql);
-        if ($query->row)
-        {
-            $result = array();
-            foreach ($query->rows as $row)
-            {
-                $row['code'] = 'dostavkaGuru.dostavkaGuru_' . $row['geo_zone_id'];
-                $row['shippingMethodName'] = 'dostavkaGuru Shipping ' . $row['name'];
-                $result[] = $row;
-            }
-            return $result;
-        }
-        else
-            return null;
-    }
+        $this->load->language('shipping/dostavkaGuru');
 
-    public function getName($languageResource = null) {
-        return parent::getName('shipping/dostavkaGuru');
-    }
-
-    public function getQuote($address) {
-		$this->load->language('shipping/dostavkaGuru');
-
-		$quote_data = array();
-		/// Assume the geo zone defined as intermediate exists
-		$geoZone = $this->getDb()->query("
+        $methodData = array();
+        /// Assume the geo zone defined as intermediate exists
+        $geoZone = $this->getDb()->query("
 			SELECT *
 			FROM geo_zone
 			WHERE geo_zone_id = :intermediateZoneId
 			", [
-				":intermediateZoneId" => $this->intermediateZoneId
-			]
-		)->row;
+                ":intermediateZoneId" => $this->intermediateZoneId
+            ]
+        )->row;
 
-		$issuePoints = $this->getIssuePoints($address);
-		foreach ($issuePoints as $issuePoint) {
-			$shippingMethod = ShippingMethodDAO::getInstance()->getMethod($this->intermediateShippingMethod);
-			$query = $this->getDb()->query("
+        $issuePoints = $this->getIssuePoints($address);
+        foreach ($issuePoints as $issuePoint) {
+            $query = $this->getDb()->query("
 				SELECT *
 				FROM zone_to_geo_zone
 				WHERE
@@ -144,96 +110,107 @@ class DostavkaGuru extends ShippingMethodBase {
 					AND country_id = :countryId
 					AND zone_id IN (:zoneId, 0)
 				", [
-					':intermediateZoneId' => $this->intermediateZoneId,
-					':countryId' => $address['country_id'],
-					':zoneId' => $address['zone_id']
-				]
-			);
+                    ':intermediateZoneId' => $this->intermediateZoneId,
+                    ':countryId' => $address['country_id'],
+                    ':zoneId' => $address['zone_id']
+                ]
+            );
 
-			if ($query->num_rows) {
-				$cost = '';
-				$weight = $this->cart->getWeight(true);
-
-				$rates = explode(',', SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'intermediateZoneRate'));
-
-				foreach ($rates as $rate) {
-					$data = explode(':', $rate);
-
-					if ($data[0] >= $weight) {
-						if (isset($data[1])) {
-							$cost = $data[1];
-						}
-
-						break;
-					}
-				}
-				$russiaCost = $this->getRussiaShippingCost($issuePoint, $weight);
-				$textRussiaCost = "<br />" . $this->getLanguage()->get('RUSSIA_DELIVERY_COST') . ": $russiaCost RUR";
-
-				if ((string)$cost != '') {
-					$quote_data['dostavkaGuru_' . $issuePoint->id] = array(
-						'code'         => 'dostavkaGuru.dostavkaGuru_' . $issuePoint->id,
-                        'description'  => $geoZone['description'],
+            if ($query->num_rows) {
+                $methodData['dostavkaGuru_' . $issuePoint->id] = array(
+                    'issuePoint' => $issuePoint,
+                    'code'         => 'dostavkaGuru.dostavkaGuru_' . $issuePoint->id,
+                    'description'  => $geoZone['description'],
 //						'title'        => $query->row['name'] . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($weight, $this->config->get('config_weight_class_id')) . ')',
-						'title'        => $issuePoint->address . ' ' .
-							$this->weight->format($weight, $this->config->get('config_weight_class_id')) .
-							$textRussiaCost,
-						'cost'         => $cost,
-						'tax_class_id' => SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'dostavkaGuruTaxClassId'),
-						'text'         => $this->currency->format($this->tax->calculate($cost, SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'dostavkaGuruTaxClassId'), $this->config->get('config_tax')))
-					);
-				}
-			}
-		}
+                    'shippingMethodName' => $this->getName() . ' ' . $issuePoint->address,
+                    'tax_class_id' => SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'dostavkaGuruTaxClassId')
+                );
+            }
+        }
 
-		$method_data = array();
+        return $methodData;
+    }
 
-		if ($quote_data) {
-      		$method_data = array(
-        		'code'       => 'dostavkaGuru',
-        		'title'      => $this->language->get('text_title'),
-        		'quote'      => $quote_data,
-				'sort_order' => $this->config->get('dostavkaGuruSortOrder'),
-        		'error'      => false
-      		);
-		}
+    public function getName() {
+        return $this->getNameByResource('shipping/dostavkaGuru');
+    }
 
-		return $method_data;
-  	}
+    public function getQuote($address) {
+        $methodData = $this->getMethodData($address);
+        foreach ($methodData as $methodDataEntry) {
+            $cost = '';
+            $weight = $this->cart->getWeight(true);
 
-	/**
-	 * @param IssuePoint $point
-	 * @param float $weight
-	 * @return float
-	 */
-	private function getRussiaShippingCost($point, $weight) {
-		$response = WebClient::getResponse(
-			'http://api.dostavka.guru/client/calc_guru_main_2_0.php',
-			'POST',
-			[
-				'client' => $this->partnerId,
-				'key' => $this->key,
-				'method' => 'ПВЗ',
-				'weight' => $weight,
+            $rates = explode(',', SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'intermediateZoneRate'));
+
+            foreach ($rates as $rate) {
+                $data = explode(':', $rate);
+
+                if ($data[0] >= $weight) {
+                    if (isset($data[1])) {
+                        $cost = $data[1];
+                    }
+
+                    break;
+                }
+            }
+            $russiaCost = $this->getRussiaShippingCost($methodDataEntry['issuePoint'], $weight);
+            $textRussiaCost = "<br />" . $this->getLanguage()->get('RUSSIA_DELIVERY_COST') . ": $russiaCost RUR";
+            $methodDataEntry['cost'] = $cost;
+            $methodDataEntry['text'] = $this->currency->format(
+                $this->tax->calculate($cost, SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'dostavkaGuruTaxClassId'), $this->config->get('config_tax')));
+            $methodDataEntry['title'] = $methodDataEntry['issuePoint']->address . ' ' .
+                $this->weight->format($weight, $this->config->get('config_weight_class_id')) .
+                $textRussiaCost;
+        }
+        $quoteData = array();
+
+        if ($methodData) {
+            $quoteData = array(
+                'code'       => 'dostavkaGuru',
+                'title'      => $this->language->get('text_title'),
+                'quote'      => $methodData,
+                'sort_order' => $this->config->get('dostavkaGuruSortOrder'),
+                'error'      => false
+            );
+        }
+
+        return $quoteData;
+    }
+
+    /**
+     * @param IssuePoint $point
+     * @param float $weight
+     * @return float
+     */
+    private function getRussiaShippingCost($point, $weight) {
+        $response = WebClient::getResponse(
+            'http://api.dostavka.guru/client/calc_guru_main_2_0.php',
+            'POST',
+            [
+                'client' => $this->partnerId,
+                'key' => $this->key,
+                'method' => 'ПВЗ',
+                'weight' => $weight,
 //				'ocen_sum' => '0',
 //				'nal_plat' => '1000',
-				'point' => $point->id
-			]
-		);
-		return preg_split('/::/', $response)[0];
-	}
+                'point' => $point->id
+            ]
+        );
+        return preg_split('/::/', $response)[0];
+    }
 
-	public function isEnabled() {
-		return boolval($this->config->get('dostavkaGuruStatus'));
-	}
+    public function isEnabled() {
+        return boolval($this->config->get('dostavkaGuruStatus'));
+    }
 
-	public function getSortOrder() {
-		return SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'dostavkaGuruSortOrder');
-	}
+    public function getSortOrder() {
+        return SettingsDAO::getInstance()->getSetting('dostavkaGuru', 'dostavkaGuruSortOrder');
+    }
 }
 
 class IssuePoint {
-	public $id;
-	public $name;
-	public $address;
+    public $id;
+    public $name;
+    public $address;
 }
--- model/shipping/ems.class.php
+++ model/shipping/ems.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace  model\shipping;
 class Ems extends ShippingMethodBase {
-  	public function getCost($destination, $orderItems, $ext = array()) {
+    public function getCost($destination, $orderItems, $ext = array()) {
 //        $this->log->write(print_r($orderItems, true));
         $cost = 0;
         $rates = explode(',', $this->config->get($destination . '_rate'));
@@ -28,8 +28,8 @@ class Ems extends ShippingMethodBase {
             }
         }
         $this->log->write($cost);
-		return $cost;
-  	}
+        return $cost;
+    }
 
     public function getMethodData($address)
     {
@@ -61,82 +61,82 @@ class Ems extends ShippingMethodBase {
             return null;
     }
 
-    public function getName($languageResource = null)
+    public function getName()
     {
-        return parent::getName('shipping/ems');
+        return $this->getNameByResource('shipping/ems');
     }
 
     public function getQuote($address) {
-		$this->load->language('shipping/ems');
+        $this->load->language('shipping/ems');
 
-		$quote_data = array();
+        $quote_data = array();
 
-		$query = $this->getDb()->query("SELECT * FROM geo_zone ORDER BY name");
+        $query = $this->getDb()->query("SELECT * FROM geo_zone ORDER BY name");
 
-		foreach ($query->rows as $result) {
-			if ($this->config->get('ems_' . $result['geo_zone_id'] . '_status')) {
-				$query = $this->getDb()->query("SELECT * FROM zone_to_geo_zone WHERE geo_zone_id = '" . (int)$result['geo_zone_id'] . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+        foreach ($query->rows as $result) {
+            if ($this->config->get('ems_' . $result['geo_zone_id'] . '_status')) {
+                $query = $this->getDb()->query("SELECT * FROM zone_to_geo_zone WHERE geo_zone_id = '" . (int)$result['geo_zone_id'] . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
-				if ($query->num_rows) {
-					$status = true;
-				} else {
-					$status = false;
-				}
-			} else {
-				$status = false;
-			}
+                if ($query->num_rows) {
+                    $status = true;
+                } else {
+                    $status = false;
+                }
+            } else {
+                $status = false;
+            }
 
-			if ($status) {
-				$cost = '';
-				$weight = $this->cart->getWeight(true);
+            if ($status) {
+                $cost = '';
+                $weight = $this->cart->getWeight(true);
 
-				$rates = explode(',', $this->config->get('ems_' . $result['geo_zone_id'] . '_rate'));
+                $rates = explode(',', $this->config->get('ems_' . $result['geo_zone_id'] . '_rate'));
 
-				foreach ($rates as $rate) {
-					$data = explode(':', $rate);
+                foreach ($rates as $rate) {
+                    $data = explode(':', $rate);
 
-					if ($data[0] >= $weight) {
-						if (isset($data[1])) {
-							$cost = $data[1];
-						}
+                    if ($data[0] >= $weight) {
+                        if (isset($data[1])) {
+                            $cost = $data[1];
+                        }
 
-						break;
-					}
-				}
+                        break;
+                    }
+                }
 
-				if ((string)$cost != '') {
-					$quote_data['ems_' . $result['geo_zone_id']] = array(
-						'code'         => 'ems.ems_' . $result['geo_zone_id'],
+                if ((string)$cost != '') {
+                    $quote_data['ems_' . $result['geo_zone_id']] = array(
+                        'code'         => 'ems.ems_' . $result['geo_zone_id'],
                         'description'  => $result['description'],
-						'title'        => $result['name'] . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($weight, $this->config->get('config_weight_class_id')) . ')',
-						'cost'         => $cost,
-						'tax_class_id' => $this->config->get('ems_tax_class_id'),
-						'text'         => $this->currency->format($this->tax->calculate($cost, $this->config->get('ems_tax_class_id'), $this->config->get('config_tax')))
-					);
-				}
-			}
-		}
+                        'title'        => $result['name'] . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($weight, $this->config->get('config_weight_class_id')) . ')',
+                        'cost'         => $cost,
+                        'tax_class_id' => $this->config->get('ems_tax_class_id'),
+                        'text'         => $this->currency->format($this->tax->calculate($cost, $this->config->get('ems_tax_class_id'), $this->config->get('config_tax')))
+                    );
+                }
+            }
+        }
 
-		$method_data = array();
+        $method_data = array();
 
-		if ($quote_data) {
-      		$method_data = array(
-        		'code'       => 'ems',
-        		'title'      => $this->language->get('text_title'),
-        		'quote'      => $quote_data,
-				'sort_order' => $this->config->get('ems_sort_order'),
-        		'error'      => false
-      		);
-		}
+        if ($quote_data) {
+            $method_data = array(
+                'code'       => 'ems',
+                'title'      => $this->language->get('text_title'),
+                'quote'      => $quote_data,
+                'sort_order' => $this->config->get('ems_sort_order'),
+                'error'      => false
+            );
+        }
 
-		return $method_data;
-  	}
+        return $method_data;
+    }
 
-	public function isEnabled() {
-		return $this->config->get('ems_status');
-	}
+    public function isEnabled() {
+        return $this->config->get('ems_status');
+    }
 
-	public function getSortOrder() {
-		return $this->config->get('ems_sort_order');
-	}
+    public function getSortOrder() {
+        return $this->config->get('ems_sort_order');
+    }
 }
