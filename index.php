95d8d6e5c5cf4e84a7ec9ecb00d536b981010281
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-05-12 09:00:27 +0200

    update: change store query

--- index.php
+++ index.php
@@ -65,9 +65,15 @@ $registry->set('db', $db);
 
 // Store
 if (isset($_SERVER['HTTPS']) && (($_SERVER['HTTPS'] == 'on') || ($_SERVER['HTTPS'] == '1'))) {
-	$store_query = $db->query("SELECT * FROM store WHERE REPLACE(`ssl`, 'www.', '') = '" . $db->escape('https://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/') . "'");
+	$store_query = $db->query("
+		SELECT * FROM store WHERE `ssl` = :url
+		", [':url' => 'https://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/']
+	);
 } else {
-	$store_query = $db->query("SELECT * FROM store WHERE REPLACE(`url`, 'www.', '') = '" . $db->escape('http://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/') . "'");
+	$store_query = $db->query("
+		SELECT * FROM store WHERE `url` = :url
+		", [':url' => 'http://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/']
+	);
 }
 if ($store_query->num_rows) {
 	$config->set('config_store_id', $store_query->row['store_id']);
