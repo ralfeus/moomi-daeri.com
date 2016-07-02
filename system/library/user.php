28c923b56cbcae058cd00b35b11c702c13bec2ae
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-07-02 19:51:32 +0200

    fix: cache invalidating

--- system/library/user.php
+++ system/library/user.php
@@ -13,26 +13,38 @@ private $usergroup_id;
 
     }
 
-  	public function __construct($registry) {
+	/**
+	 * User constructor.
+	 * @param Registry $registry
+     */
+	public function __construct($registry) {
 		$this->db = $registry->get('db');
 		$this->request = $registry->get('request');
 		$this->session = $registry->get('session');
-		
+
     	if (isset($this->session->data['user_id'])) {
 			$user_query = $this->db->query("SELECT * FROM user WHERE user_id = '" . (int)$this->session->data['user_id'] . "' AND status = '1'");
-			
+
 			if ($user_query->num_rows) {
 				$this->user_id = $user_query->row['user_id'];
 				$this->username = $user_query->row['username'];
-				
+
 /* [webme] deny order deletions by specified user_group - begin */
 $this->usergroup_id = $user_query->row['user_group_id'];
 /* [webme] deny order deletions by specified user_group - end */
 
-      			$this->db->query("UPDATE user SET ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE user_id = '" . (int)$this->session->data['user_id'] . "'");
+      			$this->db->query("
+					UPDATE user 
+					SET ip = :ip
+					WHERE user_id = :userId
+					", [
+						':ip' => $this->request->server['REMOTE_ADDR'],
+						':userId' => $this->session->data['user_id']
+					], false, true
+				);
 
       			$user_group_query = $this->db->query("SELECT permission FROM user_group WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
-				
+
 	  			$permissions = unserialize($user_group_query->row['permission']);
 
 				if (is_array($permissions)) {
@@ -45,15 +57,15 @@ $this->usergroup_id = $user_query->row['user_group_id'];
 			}
     	}
   	}
-		
+
   	public function login($username, $password) {
     	$user_query = $this->db->query("SELECT * FROM user WHERE username = '" . $this->db->escape($username) . "' AND password = '" . $this->db->escape(md5($password)) . "' AND status = '1'");
 
     	if ($user_query->num_rows) {
 			$this->session->data['user_id'] = $user_query->row['user_id'];
-			
+
 			$this->user_id = $user_query->row['user_id'];
-			$this->username = $user_query->row['username'];			
+			$this->username = $user_query->row['username'];
 
 /* [webme] deny order deletions by specified user_group - begin */
 $this->usergroup_id = $user_query->row['user_group_id'];
@@ -104,9 +116,8 @@ $this->usergroup_id = $user_query->row['user_group_id'];
     	return $this->username;
   	}	
 /* [webme] deny order deletions by specified user_group - begin */
-public function getUsergroupId() {
-return $this->usergroup_id;
-}
+	public function getUsergroupId() {
+		return $this->usergroup_id;
+	}
 /* [webme] deny order deletions by specified user_group - end */
 }
-?>
