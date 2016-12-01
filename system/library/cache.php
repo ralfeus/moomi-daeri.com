80e6e843abfb672763e154c36f8a8775a8d6f22d
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-12-01 13:45:21 +0100

    performance fix

--- system/library/cache.php
+++ system/library/cache.php
@@ -4,6 +4,7 @@ use SebastianBergmann\Exporter\Exception;
 final class Cache {
 	private $expire = 7200;
     private $logger;
+    private $destination = '/var/tmpfs';
 
   	public function __construct() {
         $this->logger = new Log("cache.log");
@@ -29,31 +30,39 @@ final class Cache {
      * @throws CacheNotInstalledException
      */
     public function get($key) {
-        if (function_exists('apc_exists')) {
-//            $this->logger->write("Trying to get '$key'");
-            if (apc_exists($key)) {
-//                $this->logger->write("\tFound '$key'");
-                return unserialize(apc_fetch($key));
-            } else {
-//                $this->logger->write("\tCouldn't find '$key'");
-                return null; //TODO: Check why it was removed
-            }
-        } else {
-//            $this->logger->write("No cache function");
-            throw new CacheNotInstalledException();
-        }
-//        else {
-//            return null;
+//        if (function_exists('apc_exists')) {
+////            $this->logger->write("Trying to get '$key'");
+//            if (apc_exists($key)) {
+////                $this->logger->write("\tFound '$key'");
+//                return unserialize(apc_fetch($key));
+//            } else {
+////                $this->logger->write("\tCouldn't find '$key'");
+//                return null; //TODO: Check why it was removed
+//            }
+//        } else {
+////            $this->logger->write("No cache function");
+//            throw new CacheNotInstalledException();
 //        }
+////        else {
+////            return null;
+////        }
+        $cacheFile = $this->destination . '/cache.' . $key;
+        if (file_exists($cacheFile)) {
+            return unserialize(file_get_contents($cacheFile));
+        } else {
+            return null;
+        }
     }
 
     public function set($key, $value) {
-        if (function_exists('apc_store')) {
-//            $this->logger->write("Setting '$key'");
-            apc_store($key, serialize($value), $this->expire);
-        } else {
-            throw new CacheNotInstalledException();
-        }
+//        if (function_exists('apc_store')) {
+////            $this->logger->write("Setting '$key'");
+//            apc_store($key, serialize($value), $this->expire);
+//        } else {
+//            throw new CacheNotInstalledException();
+//        }
+        $cacheFile = $this->destination . '/cache.' . $key;
+        file_put_contents($cacheFile, serialize($value));
     }
 
     /**
@@ -62,21 +71,30 @@ final class Cache {
      * @throws CacheNotInstalledException
      */
     public function delete($key) {
-        if (function_exists('apc_delete')) {
-            return apc_delete($key);
-        } else {
-            throw new CacheNotInstalledException();
+//        if (function_exists('apc_delete')) {
+//            return apc_delete($key);
+//        } else {
+//            throw new CacheNotInstalledException();
+//        }
+        $cacheFile = $this->destination . '/cache.' . $key;
+        if (file_exists($cacheFile)) {
+            unlink($cacheFile);
         }
     }
 
     public function deleteAll($keyPattern) {
-        if (class_exists('APCIterator')) {
-            $iterator = new APCIterator('user', $keyPattern);
-            foreach ($iterator as $entry) {
-                apc_delete($entry['key']);
+//        if (class_exists('APCIterator')) {
+//            $iterator = new APCIterator('user', $keyPattern);
+//            foreach ($iterator as $entry) {
+//                apc_delete($entry['key']);
+//            }
+//        } else {
+//            throw new CacheNotInstalledException();
+//        }
+        foreach (glob($this->destination . '/cache.*') as $cacheFile) {
+            if (preg_match($keyPattern, $cacheFile)) {
+                unlink($cacheFile);
             }
-        } else {
-            throw new CacheNotInstalledException();
         }
     }
 }
