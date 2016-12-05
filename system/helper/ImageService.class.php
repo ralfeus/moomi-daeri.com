6933f57659692935a2a70c95adec3f2ad8af5002
Mykhaylo Khodorev <ralfeus@gmail.com>
2016-12-05 10:59:58 +0100

    performance investigation: image resize

--- system/helper/ImageService.class.php
+++ system/helper/ImageService.class.php
@@ -38,22 +38,29 @@ class ImageService extends DAO {
         $extension = $info['extension'];
 
         $old_image = $filename;
-        try {$image = new Image(DIR_IMAGE . $old_image);}
-        catch (Exception $exc) {$image = new Image(DIR_IMAGE . 'no_image.jpg');}
+        if (!file_exists(DIR_IMAGE . $filename)) {
+            $filename = 'no_image.jpg';
+        }
+//        try {$image = new Image(DIR_IMAGE . $old_image);}
+//        catch (Exception $exc) {$image = new Image(DIR_IMAGE . 'no_image.jpg');}
         /// Ensure target size doesn't exceed original size
-        $ratio = $image->getWidth() / $image->getHeight();
+        $imageSize = getimagesize(DIR_IMAGE . $filename);
+        // $imageSize[0] - width
+        // $imageSize[1] - height
+//        $ratio = $image->getWidth() / $image->getHeight();
+        $ratio = $imageSize[0] / $imageSize[1];
         $expectedHeight = round($width / $ratio);
         $expectedWidth = round($height * $ratio);
-        if ($image->getWidth() >= $width) {
+        if ($imageSize[0] >= $width) {
             $targetWidth = $width;
             $targetHeight = $expectedHeight;
         } else {
-            $targetWidth = $image->getWidth();
-            $targetHeight = $image->getHeight();
+            $targetWidth = $imageSize[0];
+            $targetHeight = $imageSize[1];
         }
-        if ($image->getHeight() < $targetHeight) {
+        if ($imageSize[1] < $targetHeight) {
             $targetWidth = $expectedWidth;
-            $targetHeight = $image->getHeight();
+            $targetHeight = $imageSize[1];
         }
         $new_image = 'cache/' . substr($filename, 0, strrpos($filename, '.')) . '-' . $targetWidth . 'x' . $targetHeight . '.' . $extension;
 
@@ -69,7 +76,7 @@ class ImageService extends DAO {
                     @mkdir(DIR_IMAGE . $path, 0777);
                 }
             }
-
+            $image = new Image(DIR_IMAGE . $old_image);
             $image->resize($targetWidth, $targetHeight);
             $image->save(DIR_IMAGE . $new_image);
         }
